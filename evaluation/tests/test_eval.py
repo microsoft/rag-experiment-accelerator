17ff8f1e3b9f9063296218c97d8277163ec2a771
joshuaphelpsms <joshuaphelps@microsoft.com>
2023-10-18 10:29:39 -0700

    fix test

--- evaluation/tests/test_eval.py
+++ evaluation/tests/test_eval.py
@@ -3,57 +3,52 @@ from evaluation.eval import evaluate_search_result
 from evaluation.spacy_evaluator import SpacyEvaluator
 
 evaluation_content = "my content to evaluate"
-search_response_content = {
-    "query": "this is a query",
-    "response": [
-        {
-            "@search.score": 0.03755760192871094,
-            "content": "this is the first chunk",
-        },
-        {
-            "@search.score": 0.029906954616308212,
-            "content": "this is the second chunk",
-        },
-        {
-            "@search.score": 0.028612013906240463,
-            "content": "this is the third chunk",
-        },
-    ],
-}
+search_response = [
+    {
+        "@search.score": 0.03755760192871094,
+        "content": "this is the first chunk",
+    },
+    {
+        "@search.score": 0.029906954616308212,
+        "content": "this is the second chunk",
+    },
+    {
+        "@search.score": 0.028612013906240463,
+        "content": "this is the third chunk",
+    },
+]
 
 
 def test_evaluate_search_result_calulates_precision_score():
-    with patch('evaluation.spacy_evaluator.SpacyEvaluator') as evaluator:
+    with patch("evaluation.spacy_evaluator.SpacyEvaluator") as evaluator:
         evaluator.is_relevant.side_effect = [True, False, True]
 
         _, evaluation = evaluate_search_result(
-            search_response_content, evaluation_content, evaluator
+            search_response, evaluation_content, evaluator
         )
 
         expected_precision = ["1.0@1", "0.5@2", "0.6666666666666666@3"]
-        for i, precision in enumerate(evaluation.get('precision_scores')):
+        for i, precision in enumerate(evaluation.get("precision_scores")):
             assert precision == expected_precision[i]
 
 
 def test_evaluate_search_result_calulates_recall_score():
-    with patch('evaluation.spacy_evaluator.SpacyEvaluator') as evaluator:
+    with patch("evaluation.spacy_evaluator.SpacyEvaluator") as evaluator:
         evaluator.is_relevant.side_effect = [True, False, True]
 
         _, evaluation = evaluate_search_result(
-            search_response_content, evaluation_content, evaluator
+            search_response, evaluation_content, evaluator
         )
 
         expected_recall = ["0.5@1", "0.5@2", "1.0@3"]
-        for i, recall in enumerate(evaluation.get('recall_scores')):
+        for i, recall in enumerate(evaluation.get("recall_scores")):
             assert recall == expected_recall[i]
 
 
 def test_evaluate_search_result_returns_all_search_content():
-        evaluator = SpacyEvaluator()
+    evaluator = SpacyEvaluator()
 
-        content, _ = evaluate_search_result(
-            search_response_content, evaluation_content, evaluator
-        )
+    content, _ = evaluate_search_result(search_response, evaluation_content, evaluator)
 
-        for i, doc in enumerate(search_response_content.get('response')):
-            assert content[i] == doc["content"]
+    for i, doc in enumerate(search_response):
+        assert content[i] == doc["content"]
