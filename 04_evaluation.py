588b61a6132b07e44bb917669e02e3a523d714b6
Vadim Kirilin <Wvadim@gmail.com>
2023-10-26 10:48:58 +0100

    04_evaluation is now using config class

--- 04_evaluation.py
+++ 04_evaluation.py
@@ -4,29 +4,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from evaluation import eval
+from config.config import Config
 
-with open('search_config.json', 'r') as json_file:
-    data = json.load(json_file)
+config = Config()
 
-chunk_sizes = data["chunking"]["chunk_size"]
-overlap_size = data["chunking"]["overlap_size"]
+name_prefix = config.NAME_PREFIX
 
-embedding_dimensions = data["embedding_dimension"]
-efConstructions = data["efConstruction"]
-efsearchs = data["efsearch"]
-name_prefix = data["name_prefix"]
-search_variants = data["search_types"]
-all_index_config = "artifacts/generated_index_names"
-chat_model_name = data["chat_model_name"]
-temperature = data["openai_temperature"]
-
-for chunk_size in chunk_sizes:
-    for overlap in overlap_size:
-        for dimension in embedding_dimensions:
-            for efConstruction in efConstructions:
-                for efsearch in efsearchs:
+for chunk_size in config.CHUNK_SIZES:
+    for overlap in config.OVERLAP_SIZES:
+        for dimension in config.EMBEDDING_DIMENSIONS:
+            for efConstruction in config.EF_CONSTRUCTIONS:
+                for efsearch in config.EF_SEARCH:
                     index_name = f"{name_prefix}-{chunk_size}-{overlap}-{dimension}-{efConstruction}-{efsearch}"
                     print(f"{name_prefix}-{chunk_size}-{overlap}-{dimension}-{efConstruction}-{efsearch}")
                     write_path = f"artifacts/outputs/eval_output_{index_name}.jsonl"
-                    eval.evaluate_prompts(name_prefix, write_path, chunk_size, overlap, dimension, efConstruction,
+                    eval.evaluate_prompts(name_prefix, write_path, config, chunk_size, overlap, dimension, efConstruction,
                                           efsearch)
--- config/config.py
+++ config/config.py
@@ -5,14 +5,17 @@ class Config:
     """
     A class for storing configuration settings for the RAG Experiment Accelerator.
 
+    Parameters:
+        config_filename (str): The name of the JSON file containing configuration settings. Default is 'search_config.json'.
+
     Attributes:
-        CHUNK_SIZES (list): A list of integers representing the chunk sizes for chunking documents.
-        OVERLAP_SIZES (list): A list of integers representing the overlap sizes for chunking documents.
-        EMBEDDING_DIMENSIONS (int): The number of dimensions to use for document embeddings.
-        EF_CONSTRUCTIONS (int): The number of efConstructions to use for HNSW index.
-        EF_SEARCH (int): The number of efSearch to use for HNSW index.
+        CHUNK_SIZES (list[int]): A list of integers representing the chunk sizes for chunking documents.
+        OVERLAP_SIZES (list[int]): A list of integers representing the overlap sizes for chunking documents.
+        EMBEDDING_DIMENSIONS (list[int]): The number of dimensions to use for document embeddings.
+        EF_CONSTRUCTIONS (list[int]): The number of efConstructions to use for HNSW index.
+        EF_SEARCH (list[int]): The number of efSearch to use for HNSW index.
         NAME_PREFIX (str): A prefix to use for the names of saved models.
-        SEARCH_VARIANTS (list): A list of search types to use.
+        SEARCH_VARIANTS (list[str]): A list of search types to use.
         CHAT_MODEL_NAME (str): The name of the chat model to use.
         RETRIEVE_NUM_OF_DOCUMENTS (int): The number of documents to retrieve for each query.
         CROSSENCODER_MODEL (str): The name of the crossencoder model to use.
@@ -22,9 +25,10 @@ class Config:
         TEMPERATURE (float): The temperature to use for OpenAI's GPT-3 model.
         RERANK (bool): Whether or not to perform reranking.
         SEARCH_RELEVANCY_THRESHOLD (float): The threshold for search result relevancy.
+        METRIC_TYPES (list[str]): A list of metric types to use.
     """
-    def __init__(self) -> None:
-        with open('search_config.json', 'r') as json_file:
+    def __init__(self, config_filename: str = 'search_config.json') -> None:
+        with open(config_filename, 'r') as json_file:
             data = json.load(json_file)
 
         self.CHUNK_SIZES = data["chunking"]["chunk_size"]
@@ -43,3 +47,4 @@ class Config:
         self.TEMPERATURE = data["openai_temperature"]
         self.RERANK = data['rerank']
         self.SEARCH_RELEVANCY_THRESHOLD = data.get("search_relevancy_threshold", 0.8)
+        self.METRIC_TYPES = data["metric_types"]
