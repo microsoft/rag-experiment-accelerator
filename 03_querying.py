ba8bec071aa45226600b7a0ab7e25202f0cda176
auyidi1 <107195562+auyidi1@users.noreply.github.com>
2023-10-26 04:17:52 -0700

    Check for eval data json file existence

--- 03_querying.py
+++ 03_querying.py
@@ -112,106 +112,109 @@ def query_and_eval_acs_multi(
 def main(config: Config):
     jsonl_file_path = config.EVAL_DATA_JSON_FILE_PATH
     question_count = 0
-    with open(jsonl_file_path, 'r') as file:
-        for line in file:
-            question_count += 1
+    try:
+        with open(jsonl_file_path, 'r') as file:
+            for line in file:
+                question_count += 1             
 
-    if (config.MAIN_PROMPT_INSTRUCTIONS):
-        main_prompt_instruction = config.MAIN_PROMPT_INSTRUCTIONS
-    else:
-        main_prompt_instruction = llm.prompts.main_prompt_instruction
+        if (config.MAIN_PROMPT_INSTRUCTIONS):
+            main_prompt_instruction = config.MAIN_PROMPT_INSTRUCTIONS
+        else:
+            main_prompt_instruction = llm.prompts.main_prompt_instruction
 
-    directory_path = 'artifacts/outputs'
-    if not os.path.exists(directory_path):
-        os.makedirs(directory_path)
+        directory_path = 'artifacts/outputs'
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path)
 
-    evaluator = SpacyEvaluator(config.SEARCH_RELEVANCY_THRESHOLD)
+        evaluator = SpacyEvaluator(config.SEARCH_RELEVANCY_THRESHOLD)
 
-    for config_item in config.CHUNK_SIZES:
-        for overlap in config.OVERLAP_SIZES:
-            for dimension in config.EMBEDDING_DIMENSIONS:
-                for efConstruction in config.EF_CONSTRUCTIONS:
-                    for efsearch in config.EF_SEARCH:
-                        index_name = f"{config.NAME_PREFIX}-{config_item}-{overlap}-{dimension}-{efConstruction}-{efsearch}"
-                        print(f"Index: {index_name}")
+        for config_item in config.CHUNK_SIZES:
+            for overlap in config.OVERLAP_SIZES:
+                for dimension in config.EMBEDDING_DIMENSIONS:
+                    for efConstruction in config.EF_CONSTRUCTIONS:
+                        for efsearch in config.EF_SEARCH:
+                            index_name = f"{config.NAME_PREFIX}-{config_item}-{overlap}-{dimension}-{efConstruction}-{efsearch}"
+                            print(f"Index: {index_name}")
 
-                        write_path = f"artifacts/outputs/eval_output_{index_name}.jsonl"
-                        if os.path.exists(write_path):
-                            continue
+                            write_path = f"artifacts/outputs/eval_output_{index_name}.jsonl"
+                            if os.path.exists(write_path):
+                                continue
 
-                        search_client, index_client = create_client(service_endpoint, index_name, search_admin_key)
+                            search_client, index_client = create_client(service_endpoint, index_name, search_admin_key)
 
-                        with open(jsonl_file_path, 'r') as file:
-                            for line in file:
-                                data = json.loads(line)
-                                user_prompt = data.get("user_prompt")
-                                output_prompt = data.get("output_prompt")
-                                qna_context = data.get("context", "")
+                            with open(jsonl_file_path, 'r') as file:
+                                for line in file:
+                                    data = json.loads(line)
+                                    user_prompt = data.get("user_prompt")
+                                    output_prompt = data.get("output_prompt")
+                                    qna_context = data.get("context", "")
 
-                                is_multi_question = do_we_need_multiple_questions(user_prompt, config.CHAT_MODEL_NAME, config.TEMPERATURE)
-                                if is_multi_question:
-                                    new_questions = json.loads(we_need_multiple_questions(user_prompt, config.CHAT_MODEL_NAME, config.TEMPERATURE))['questions']
-                                    new_questions.append(user_prompt)
-
-                                evaluation_content = user_prompt + qna_context
-                                for s_v in config.SEARCH_VARIANTS:
-
-                                    search_evals = []
+                                    is_multi_question = do_we_need_multiple_questions(user_prompt, config.CHAT_MODEL_NAME, config.TEMPERATURE)
                                     if is_multi_question:
-                                        docs, search_evals = query_and_eval_acs_multi(
-                                            search_client,
-                                            dimension,
-                                            new_questions,
-                                            user_prompt,
-                                            output_prompt,
-                                            s_v,
-                                            evaluation_content,
-                                            config,
-                                            evaluator,
-                                            main_prompt_instruction
-                                        )
-                                    else:
-                                        docs, evaluation = query_and_eval_acs(
-                                            search_client,
-                                            dimension,
-                                            user_prompt,
-                                            s_v,
-                                            evaluation_content,
-                                            config.RETRIEVE_NUM_OF_DOCUMENTS,
-                                            evaluator
-                                        )
-                                        search_evals.append(evaluation)
+                                        new_questions = json.loads(we_need_multiple_questions(user_prompt, config.CHAT_MODEL_NAME, config.TEMPERATURE))['questions']
+                                        new_questions.append(user_prompt)
 
-                                    if config.RERANK:
-                                        prompt_instruction_context = rerank_documents(docs, user_prompt, output_prompt, config)
-                                    else:
-                                        prompt_instruction_context = docs
+                                    evaluation_content = user_prompt + qna_context
+                                    for s_v in config.SEARCH_VARIANTS:
 
-                                    full_prompt_instruction = main_prompt_instruction + "\n" + "\n".join(prompt_instruction_context)
-                                    openai_response = generate_response(full_prompt_instruction,user_prompt,config.CHAT_MODEL_NAME, config.TEMPERATURE)
-                                    print(openai_response)
+                                        search_evals = []
+                                        if is_multi_question:
+                                            docs, search_evals = query_and_eval_acs_multi(
+                                                search_client,
+                                                dimension,
+                                                new_questions,
+                                                user_prompt,
+                                                output_prompt,
+                                                s_v,
+                                                evaluation_content,
+                                                config,
+                                                evaluator,
+                                                main_prompt_instruction
+                                            )
+                                        else:
+                                            docs, evaluation = query_and_eval_acs(
+                                                search_client,
+                                                dimension,
+                                                user_prompt,
+                                                s_v,
+                                                evaluation_content,
+                                                config.RETRIEVE_NUM_OF_DOCUMENTS,
+                                                evaluator
+                                            )
+                                            search_evals.append(evaluation)
 
-                                    output = {
-                                        "rerank": config.RERANK,
-                                        "rerank_type": config.RERANK_TYPE,
-                                        "crossencoder_model": config.CROSSENCODER_MODEL,
-                                        "llm_re_rank_threshold": config.LLM_RERANK_THRESHOLD,
-                                        "retrieve_num_of_documents": config.RETRIEVE_NUM_OF_DOCUMENTS,
-                                        "cross_encoder_at_k": config.CROSSENCODER_AT_K,
-                                        "question_count": question_count,
-                                        'actual': openai_response,
-                                        'expected': output_prompt,
-                                        'search_type': s_v,
-                                        'search_evals': search_evals
-                                    }
+                                        if config.RERANK:
+                                            prompt_instruction_context = rerank_documents(docs, user_prompt, output_prompt, config)
+                                        else:
+                                            prompt_instruction_context = docs
 
-                                    with open(write_path, 'a') as out:
-                                        json_string = json.dumps(output)
-                                        out.write(json_string + "\n")
+                                        full_prompt_instruction = main_prompt_instruction + "\n" + "\n".join(prompt_instruction_context)
+                                        openai_response = generate_response(full_prompt_instruction,user_prompt,config.CHAT_MODEL_NAME, config.TEMPERATURE)
+                                        print(openai_response)
 
-                        search_client.close()
-                        index_client.close()
-                        create_data_asset(write_path, index_name)
+                                        output = {
+                                            "rerank": config.RERANK,
+                                            "rerank_type": config.RERANK_TYPE,
+                                            "crossencoder_model": config.CROSSENCODER_MODEL,
+                                            "llm_re_rank_threshold": config.LLM_RERANK_THRESHOLD,
+                                            "retrieve_num_of_documents": config.RETRIEVE_NUM_OF_DOCUMENTS,
+                                            "cross_encoder_at_k": config.CROSSENCODER_AT_K,
+                                            "question_count": question_count,
+                                            'actual': openai_response,
+                                            'expected': output_prompt,
+                                            'search_type': s_v,
+                                            'search_evals': search_evals
+                                        }
+
+                                        with open(write_path, 'a') as out:
+                                            json_string = json.dumps(output)
+                                            out.write(json_string + "\n")
+
+                            search_client.close()
+                            index_client.close()
+                            create_data_asset(write_path, index_name)
+    except FileNotFoundError:
+        print('The file does not exist: ' + jsonl_file_path)                           
 
 
 if __name__ == '__main__':
