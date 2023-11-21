15e1212fea52880a1e6744771a8ff6329db0ae49
joshuaphelpsms <joshuaphelps@microsoft.com>
2023-11-21 23:38:57 +0000

    format

--- rag_experiment_accelerator/ingest_data/acs_ingest.py
+++ rag_experiment_accelerator/ingest_data/acs_ingest.py
@@ -151,33 +151,31 @@ def generate_qna(docs, model_name, temperature):
     for i, chunk in enumerate(docs):
         if len(chunk.page_content) > 50:
             response = generate_response(
-                generate_qna_instruction_system_prompt, generate_qna_instruction_user_prompt + chunk.page_content + "\nEND OF CONTEXT", model_name, temperature
+                generate_qna_instruction_system_prompt,
+                generate_qna_instruction_user_prompt
+                + chunk.page_content
+                + "\nEND OF CONTEXT",
+                model_name,
+                temperature,
             )
             try:
                 response_dict = json.loads(response)
                 for item in response_dict:
-                    if item.get("role") == "user":
+                    if item["role"] == "user":
                         user_prompt = item["content"]
-                    if item.get("role") == "assistant":
+                    if item["role"] == "assistant":
                         output_prompt = item["content"]
-                    
-                if user_prompt is not None and output_prompt is not None:
-                    data = {
-                        "user_prompt": user_prompt,
-                        "output_prompt": output_prompt,
-                        "context": chunk.page_content,
-                    }
-                    new_df = new_df._append(data, ignore_index=True)
-                    logger.info(
-                        f"Generated QnA for document {i}"
-                    )
-                else:
-                    logger.error(
-                    f"User Prompt: {user_prompt} and Output Prompt: {output_prompt}"
-                )
+
+                data = {
+                    "user_prompt": user_prompt,
+                    "output_prompt": output_prompt,
+                    "context": chunk.page_content,
+                }
+                new_df = new_df._append(data, ignore_index=True)
+                logger.info(f"Generated QnA for document {i}")
             except Exception as e:
                 logger.error(
-                    "could not generate a valid json so moving over to next question !"
+                    "could not generate a valid json so moving over to next question!"
                 )
                 logger.debug(e)
                 logger.debug(f"LLM Response: {response}")
