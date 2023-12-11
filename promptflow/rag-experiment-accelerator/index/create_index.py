33ec8a8d0698d550072eac8910815092b40b8e2d
joshuaphelpsms <joshuaphelps@microsoft.com>
2023-12-11 22:17:43 +0000

    use config over search_config

--- promptflow/rag-experiment-accelerator/index/create_index.py
+++ promptflow/rag-experiment-accelerator/index/create_index.py
@@ -3,10 +3,10 @@ from rag_experiment_accelerator.run.index import run
 
 
 @tool
-def my_python_tool(should_index: bool, search_config_dir: str) -> bool:
+def my_python_tool(should_index: bool, config_dir: str) -> bool:
     if should_index:
         try:
-            run(search_config_dir)
+            run(config_dir)
         except Exception as e:
             print(e)
             return False
--- promptflow/rag-experiment-accelerator/qa_generation/generate_qa.py
+++ promptflow/rag-experiment-accelerator/qa_generation/generate_qa.py
@@ -3,9 +3,9 @@ from rag_experiment_accelerator.run.qa_generation import run
 
 
 @tool
-def my_python_tool(search_config_dir: str) -> bool:
+def my_python_tool(config_dir: str) -> bool:
     try:
-        run(search_config_dir)
+        run(config_dir)
     except Exception as e:
         print(e)
         return False
