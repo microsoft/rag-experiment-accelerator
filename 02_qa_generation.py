2b8e89251e520a77331e48003cb182c857597694
Joshua Phelps <88395136+joshuaphelpsms@users.noreply.github.com>
2023-12-12 11:55:35 -0800

    merge Feature/promptflow with development (#198)
    * Merge development into feature/promptflow (#196)
    ---------
    
    Co-authored-by: kjeffc <jeffrey159@gmail.com>
    Co-authored-by: Anastasia Linzbach <anastasia.linzbach@gmail.com>
    Co-authored-by: anastasia-linzbach <151688308+anastasia-linzbach@users.noreply.github.com>
    
    * Feature 153 - initial promptflow dags (#183)
    
    * intital prompt flow
    
    * update eval filepath
    
    * update promptflow tools
    
    ---------
    
    Co-authored-by: kjeffc <jeffrey159@gmail.com>
    Co-authored-by: Anastasia Linzbach <anastasia.linzbach@gmail.com>
    Co-authored-by: anastasia-linzbach <151688308+anastasia-linzbach@users.noreply.github.com>
    
    * rm pormptflow branch from ci
    
    ---------
    
    Co-authored-by: kjeffc <jeffrey159@gmail.com>
    Co-authored-by: Anastasia Linzbach <anastasia.linzbach@gmail.com>
    Co-authored-by: anastasia-linzbach <151688308+anastasia-linzbach@users.noreply.github.com>

--- 02_qa_generation.py
+++ 02_qa_generation.py
@@ -1,25 +1,5 @@
-from rag_experiment_accelerator.config import Config
-
-from dotenv import load_dotenv
-
-load_dotenv(override=True)
-
-from rag_experiment_accelerator.doc_loader.documentLoader import load_documents
-from rag_experiment_accelerator.ingest_data.acs_ingest import generate_qna
 from rag_experiment_accelerator.run.args import get_directory_arg
-
-
-def run(config_dir: str):
-    """
-    Runs the main experiment loop for the QA generation process using the provided configuration and data.
-
-    Returns:
-        None
-    """
-    config = Config(config_dir)
-    all_docs = load_documents(config.DATA_FORMATS, config.data_dir, 2000, 0)
-    generate_qna(all_docs, config.CHAT_MODEL_NAME, config.TEMPERATURE, config.EVAL_DATA_JSONL_FILE_PATH)
-
+from rag_experiment_accelerator.run.qa_generation import run
 
 if __name__ == "__main__":
     directory = get_directory_arg()
