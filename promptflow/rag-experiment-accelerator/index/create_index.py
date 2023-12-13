e43058e96a34c7a5a2120665923c3259f52a313e
kjeffc <jeffrey159@gmail.com>
2023-12-13 20:02:31 +0000

    remove newline

--- promptflow/rag-experiment-accelerator/index/create_index.py
+++ promptflow/rag-experiment-accelerator/index/create_index.py
@@ -9,6 +9,5 @@ def my_python_tool(should_index: bool, config_dir: str) -> bool:
             run(config_dir)
         except Exception as e:
             print(e)
-
             return False	
     return True	
--- promptflow/rag-experiment-accelerator/querying/querying.py
+++ promptflow/rag-experiment-accelerator/querying/querying.py
@@ -8,6 +8,5 @@ def my_python_tool(config_dir: str) -> bool:
         run(config_dir)
     except Exception as e:
         print(e)
-        
         return False	
     return True
