47302d2ad6a97d9a926d6f9d000f0685d2832b65
joshuaphelpsms <joshuaphelps@microsoft.com>
2023-12-14 17:12:22 +0000

    enable flows to run standalone

--- promptflow/rag-experiment-accelerator/qa_generation/generate_qa.py
+++ promptflow/rag-experiment-accelerator/qa_generation/generate_qa.py
@@ -4,9 +4,5 @@ from rag_experiment_accelerator.run.qa_generation import run
 
 @tool
 def my_python_tool(config_dir: str) -> bool:
-    try:
-        run(config_dir)
-    except Exception as e:
-        print(e)
-        return False
+    run(config_dir)
     return True
--- rag_experiment_accelerator/run/qa_generation.py
+++ rag_experiment_accelerator/run/qa_generation.py
@@ -1,3 +1,4 @@
+import os
 from rag_experiment_accelerator.config import Config
 
 from dotenv import load_dotenv
@@ -7,6 +8,9 @@ load_dotenv(override=True)
 from rag_experiment_accelerator.doc_loader.documentLoader import load_documents
 from rag_experiment_accelerator.ingest_data.acs_ingest import generate_qna
 from rag_experiment_accelerator.run.args import get_directory_arg
+from rag_experiment_accelerator.utils.logging import get_logger
+
+logger = get_logger(__name__)
 
 
 def run(config_dir: str):
@@ -18,4 +22,11 @@ def run(config_dir: str):
     """
     config = Config(config_dir)
     all_docs = load_documents(config.DATA_FORMATS, config.data_dir, 2000, 0)
+
+    try:
+        os.makedirs(config.artifacts_dir, exist_ok=True)
+    except Exception as e:
+        logger.error(f"Unable to create the '{config.artifacts_dir}' directory. Please ensure you have the proper permissions and try again")
+        raise e
+    
     generate_qna(all_docs, config.CHAT_MODEL_NAME, config.TEMPERATURE, config.EVAL_DATA_JSONL_FILE_PATH)
