74862ef3d7610f419074d0658fac5c3801118287
Yomi Lajide <yolajide@microsoft.com>
2024-01-11 13:57:27 -0500

    update acs_ingest testing

--- rag_experiment_accelerator/ingest_data/tests/test_acs_ingest.py
+++ rag_experiment_accelerator/ingest_data/tests/test_acs_ingest.py
@@ -1,18 +1,20 @@
 import json
 
-from unittest.mock import patch, Mock
+from unittest.mock import MagicMock, patch, Mock
 from rag_experiment_accelerator.ingest_data.acs_ingest import (
     generate_title,
     my_hash,
     generate_summary,
     upload_data,
     generate_qna,
+    we_need_multiple_questions,
+    do_we_need_multiple_questions,
 )
 from rag_experiment_accelerator.llm.prompts import (
     prompt_instruction_title,
     prompt_instruction_summary,
+    multiple_prompt_instruction,
 )
-from azure.core.credentials import AzureKeyCredential
 
 
 def test_my_hash_with_string():
@@ -57,15 +59,13 @@ def test_my_hash_with_numbers():
     assert result == expected_hash
 
 
-@patch(
-    "rag_experiment_accelerator.llm.response_generator.ResponseGenerator.generate_response"
-)
-def test_generate_title(mock_generate_response):
+@patch("rag_experiment_accelerator.ingest_data.acs_ingest.ResponseGenerator")
+def test_generate_title(mock_response_generator):
     # Arrange
     mock_response = "Test Title"
     mock_chunk = "This is a test chunk of text."
     mock_deployment_name = "TestDeployment"
-    mock_generate_response.return_value = mock_response
+    mock_response_generator().generate_response.return_value = mock_response
 
     # Act
     result = generate_title(mock_chunk, mock_deployment_name)
@@ -75,93 +75,88 @@ def test_generate_title(mock_generate_response):
     # MockResponseGenerator.assert_called_once_with(
     #     deployment_name=mock_deployment_name
     # )
-    mock_generate_response.assert_called_once_with(prompt_instruction_title, mock_chunk)
+    mock_response_generator().generate_response.assert_called_once_with(
+        prompt_instruction_title, mock_chunk
+    )
 
 
-@patch(
-    "rag_experiment_accelerator.llm.response_generator.ResponseGenerator.generate_response"
-)
-def test_generate_summary(mock_generate_response):
+@patch("rag_experiment_accelerator.ingest_data.acs_ingest.ResponseGenerator")
+def test_generate_summary(mock_response_generator):
     # Arrange
     mock_summary = "Test Summary"
     mock_chunk = "This is a test chunk of text."
     mock_deployment_name = "TestDeployment"
-    mock_generate_response.return_value = mock_summary
+    mock_response_generator().generate_response.return_value = mock_summary
 
     # Act
     result = generate_summary(mock_chunk, mock_deployment_name)
 
     # Assert
     assert result == mock_summary
-    # MockResponseGenerator.assert_called_once_with(
-    #     deployment_name=mock_deployment_name
-    # )
-    mock_generate_response.assert_called_once_with(
+
+    mock_response_generator().generate_response.assert_called_once_with(
         prompt_instruction_summary, mock_chunk
     )
 
 
-# @patch("rag_experiment_accelerator.ingest_data.acs_ingest.SearchClient")
-# @patch("rag_experiment_accelerator.ingest_data.acs_ingest.AzureKeyCredential")
-# @patch("rag_experiment_accelerator.ingest_data.acs_ingest.generate_title")
-# @patch("rag_experiment_accelerator.ingest_data.acs_ingest.generate_summary")
-# @patch("rag_experiment_accelerator.ingest_data.acs_ingest.my_hash")
-# @patch("rag_experiment_accelerator.ingest_data.acs_ingest.pre_process.preprocess")
-# def test_upload_data(
-#     self,
-#     mock_preprocess,
-#     mock_my_hash,
-#     mock_generate_summary,
-#     mock_generate_title,
-#     mock_AzureKeyCredential,
-#     mock_SearchClient,
-# ):
-#     # Arrange
-#     mock_chunks = [{"content": "test content", "content_vector": "test_vector"}]
-#     mock_service_endpoint = "test_endpoint"
-#     mock_index_name = "test_index"
-#     mock_search_key = "test_key"
-#     mock_embedding_model = Mock()
-#     mock_azure_oai_deployment_name = "test_deployment"
-#     mock_my_hash.return_value = "test_hash"
-#     mock_generate_title.return_value = "test_title"
-#     mock_generate_summary.return_value = "test_summary"
-#     mock_preprocess.return_value = "test_preprocessed_content"
-#     mock_embedding_model.generate_embedding.return_value = "test_embedding"
-#     mock_search_client = Mock()
-#     mock_SearchClient.return_value = mock_search_client
-#     mock_AzureKeyCredential.return_value = "test_credential"
+@patch("rag_experiment_accelerator.ingest_data.acs_ingest.SearchClient")
+@patch("rag_experiment_accelerator.ingest_data.acs_ingest.AzureKeyCredential")
+@patch("rag_experiment_accelerator.ingest_data.acs_ingest.generate_summary")
+@patch("rag_experiment_accelerator.ingest_data.acs_ingest.generate_title")
+@patch("rag_experiment_accelerator.ingest_data.acs_ingest.my_hash")
+@patch("rag_experiment_accelerator.ingest_data.acs_ingest.pre_process.preprocess")
+def test_upload_data(
+    mock_preprocess,
+    mock_my_hash,
+    mock_generate_summary,
+    mock_generate_title,
+    mock_AzureKeyCredential,
+    mock_SearchClient,
+):
+    # Arrange
+    mock_chunks = [{"content": "test content", "content_vector": "test_vector"}]
+    mock_service_endpoint = "test_endpoint"
+    mock_index_name = "test_index"
+    mock_search_key = "test_key"
+    mock_embedding_model = Mock()
+    mock_azure_oai_deployment_name = "test_deployment"
+    mock_my_hash.return_value = "test_hash"
+    mock_generate_title.return_value = "test_title"
+    mock_generate_summary.return_value = "test_summary"
+    mock_preprocess.return_value = "test_preprocessed_content"
+    mock_AzureKeyCredential.return_value = "test_credential"
 
-#     # Act
-#     upload_data(
-#         mock_chunks,
-#         mock_service_endpoint,
-#         mock_index_name,
-#         mock_search_key,
-#         mock_embedding_model,
-#         mock_azure_oai_deployment_name,
-#     )
+    # Act
+    upload_data(
+        mock_chunks,
+        mock_service_endpoint,
+        mock_index_name,
+        mock_search_key,
+        mock_embedding_model,
+        mock_azure_oai_deployment_name,
+    )
 
-#     # Assert
-#     mock_AzureKeyCredential.assert_called_once_with(mock_search_key)
-#     mock_SearchClient.assert_called_once_with(
-#         endpoint=mock_service_endpoint,
-#         index_name=mock_index_name,
-#         credential="test_credential",
-#     )
-#     mock_my_hash.assert_called_once_with(mock_chunks[0]["content"])
-#     mock_generate_title.assert_called_once_with(
-#         str(mock_chunks[0]["content"]), mock_azure_oai_deployment_name
-#     )
-#     mock_generate_summary.assert_called_once_with(
-#         str(mock_chunks[0]["content"]), mock_azure_oai_deployment_name
-#     )
-#     mock_preprocess.assert_any_call("test_summary")
-#     mock_preprocess.assert_any_call("test_title")
-#     mock_embedding_model.generate_embedding.assert_any_call(
-#         chunk="test_preprocessed_content"
-#     )
-#     mock_search_client.upload_documents.assert_called_once()
+    # Assert
+    mock_AzureKeyCredential.assert_called_once_with(mock_search_key)
+    mock_SearchClient.assert_called_once_with(
+        endpoint=mock_service_endpoint,
+        index_name=mock_index_name,
+        credential="test_credential",
+    )
+    mock_my_hash.assert_called_once_with(mock_chunks[0]["content"])
+    mock_generate_title.assert_called_once_with(
+        str(mock_chunks[0]["content"]), mock_azure_oai_deployment_name
+    )
+    mock_generate_summary.assert_called_once_with(
+        str(mock_chunks[0]["content"]), mock_azure_oai_deployment_name
+    )
+    mock_preprocess.assert_any_call("test_summary")
+    mock_preprocess.assert_any_call("test_title")
+    mock_embedding_model.generate_embedding.assert_any_call(
+        chunk="test_preprocessed_content"
+    )
+    assert mock_embedding_model.generate_embedding.call_count == 2
+    mock_SearchClient().upload_documents.assert_called_once()
 
 
 @patch(
@@ -226,9 +221,70 @@ def test_generate_qna_with_invalid_json(mock_json_loads, mock_generate_response)
     # mock_logger.debug.assert_called()
 
 
-def test_we_need_multiple_questions():
-    assert False
+@patch("rag_experiment_accelerator.ingest_data.acs_ingest.ResponseGenerator")
+def test_we_need_multiple_questions(mock_response_generator):
+    # Arrange
+    question = "What is the meaning of life?"
+    azure_oai_deployment_name = "TestDeployment"
+    mock_response = "The meaning of life is 42."
+    mock_response_generator().generate_response.return_value = mock_response
+    expected_prompt_instruction = (
+        multiple_prompt_instruction + "\n" + "question: " + question + "\n"
+    )
+
+    # Act
+    result = we_need_multiple_questions(question, azure_oai_deployment_name)
+
+    # Assert
+    mock_response_generator.assert_called_once_with(
+        deployment_name=azure_oai_deployment_name
+    )
+    mock_response_generator().generate_response.assert_called_once_with(
+        expected_prompt_instruction, ""
+    )
+    assert result == mock_response
 
 
-def test_do_we_need_multiple_questions():
-    assert False
+@patch("rag_experiment_accelerator.ingest_data.acs_ingest.ResponseGenerator")
+def test_do_we_need_multiple_questions_high(mock_response_generator):
+    # Arrange
+    question = "What is the meaning of life?"
+    azure_oai_deployment_name = "TestDeployment"
+    mock_response_generator().generate_response.return_value = "HIGH"
+
+    # Act
+    result = do_we_need_multiple_questions(question, azure_oai_deployment_name)
+
+    # Assert
+    assert result is not None
+    mock_response_generator().generate_response.assert_called_once()
+
+
+@patch("rag_experiment_accelerator.ingest_data.acs_ingest.ResponseGenerator")
+def test_do_we_need_multiple_questions_empty(mock_response_generator):
+    # Arrange
+    question = "What is the meaning of life?"
+    azure_oai_deployment_name = "TestDeployment"
+    mock_response_generator().generate_response.return_value = ""
+
+    # Act
+    result = do_we_need_multiple_questions(question, azure_oai_deployment_name)
+
+    # Assert
+    assert result is None
+    mock_response_generator().generate_response.assert_called_once()
+
+
+@patch("rag_experiment_accelerator.ingest_data.acs_ingest.ResponseGenerator")
+def test_do_we_need_multiple_questions_low(mock_response_generator):
+    # Arrange
+    question = "What is the meaning of life?"
+    azure_oai_deployment_name = "TestDeployment"
+    mock_response_generator().generate_response.return_value = "LOW"
+
+    # Act
+    result = do_we_need_multiple_questions(question, azure_oai_deployment_name)
+
+    # Assert
+    assert result is None
+    mock_response_generator().generate_response.assert_called_once()
