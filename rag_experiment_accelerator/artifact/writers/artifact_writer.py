19ad9484942a390dc4eacd7ea3b52ebdcbc2780c
joshuaphelpsms <joshuaphelps@microsoft.com>
2024-01-12 08:17:33 +0000

    rm comments

--- rag_experiment_accelerator/artifact/writers/artifact_writer.py
+++ rag_experiment_accelerator/artifact/writers/artifact_writer.py
@@ -20,11 +20,6 @@ class ArtifactWriter(Generic[T]):
         src = f"{self.directory}/{filename}"
         # archive if file exists, else no-op
         if self._writer.exists(src):
-            # set up directory structure
-            # if self._artifact_dir_initialized is False:
-            #     self._writer.prepare_write(self.archive_dir)
-            #     self._artifact_dir_initialized = True
-
             # timestamp filename in archive dir
             timestamp = time.strftime("%Y%m%d-%H%M%S")
             dest = f"{self.archive_dir}/{timestamp}-{filename}"
@@ -39,11 +34,5 @@ class ArtifactWriter(Generic[T]):
             return dest
 
     def save_artifact(self, data: T, filename: str, **kwargs) -> None:
-        # set up directory structure
-        # if self._directory_structure_initialized is False:
-        #     self._writer.prepare_write(self.directory)
-        #     self._directory_structure_initialized = True
-
-        # write file
         path = f"{self.directory}/{filename}"
         self._writer.write(path, data.to_dict(), **kwargs)
