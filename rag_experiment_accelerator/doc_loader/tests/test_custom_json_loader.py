e1bcae7402369410238dc493d8c047f0698acd56
joshuaphelpsms <joshuaphelps@microsoft.com>
2024-01-17 00:20:21 +0000

    remove langchain JSONLoader

--- rag_experiment_accelerator/doc_loader/tests/test_custom_json_loader.py
+++ rag_experiment_accelerator/doc_loader/tests/test_custom_json_loader.py
@@ -1,4 +1,5 @@
-from langchain.document_loaders import JSONLoader
+import os
+import pytest
 
 from rag_experiment_accelerator.doc_loader.customJsonLoader import (
     CustomJSONLoader,
@@ -8,41 +9,83 @@ from rag_experiment_accelerator.doc_loader.structuredLoader import (
 )
 
 
-# The output of our customJsonLoader should be equal to the output of langchains JSONLoader
 def test_load_json_files():
-    folder_path = "./data/"
-    chunk_size = 1000
-    overlap_size = 200
-
-    original_doc = load_structured_files(
-        file_format="JSON",
-        language=None,
-        loader=JSONLoader,
-        folder_path=folder_path,
-        chunk_size=chunk_size,
-        overlap_size=overlap_size,
-        glob_patterns=["json"],
-        loader_kwargs={
-            "jq_schema": "[.[] | {content: .content, title: .title}]",
-            "text_content": False,
-        },
-    )
-
-    custom_doc = load_structured_files(
+    keys_to_load = ["content", "title"]
+    docs = load_structured_files(
         file_format="JSON",
         language=None,
         loader=CustomJSONLoader,
-        folder_path=folder_path,
-        chunk_size=chunk_size,
-        overlap_size=overlap_size,
-        glob_patterns=["json"],
+        folder_path="rag_experiment_accelerator/doc_loader/tests/test_data/json",
+        chunk_size=1000,
+        overlap_size=200,
+        glob_patterns=["good.json"],
         loader_kwargs={
-            "jq_schema": "[.[] | {content: .content, title: .title}]",
-            "text_content": False,
+            "keys_to_load": keys_to_load,
         },
     )
+    assert (
+        docs[0].page_content
+        == "[{'content': 'This is the content for item 1.', 'title': 'Title TEST 1'}, {'content': 'This is the content for item 2.', 'title': 'Title 2'}, {'content': 'This is the content for item 3.', 'title': 'Title 3'}, {'content': 'This is the content for item 4.', 'title': 'Title 4'}, {'content': 'This is the content for item 5.', 'title': 'Title 5'}, {'content': 'This is the content for item 6.', 'title': 'Title 6'}]"
+    )
+    assert (
+        docs[0]
+        .metadata["source"]
+        .endswith(
+            "rag_experiment_accelerator/doc_loader/tests/test_data/json/data.good.json"
+        )
+    )
 
-    assert len(original_doc) == len(custom_doc)
 
-    assert original_doc[0].page_content == custom_doc[0].page_content
-    assert original_doc[0].metadata == custom_doc[0].metadata
+def test_load_json_files_raises_invalid_keys():
+    keys_to_load = ["content", "title"]
+    with pytest.raises(ValueError) as exec_info:
+        load_structured_files(
+            file_format="JSON",
+            language=None,
+            loader=CustomJSONLoader,
+            folder_path=os.path.abspath(
+                "rag_experiment_accelerator/doc_loader/tests/test_data/json"
+            ),
+            chunk_size=1000,
+            overlap_size=200,
+            glob_patterns=["invalid_keys.json"],
+            loader_kwargs={
+                "keys_to_load": keys_to_load,
+            },
+        )
+
+    file_path = os.path.abspath(
+        "rag_experiment_accelerator/doc_loader/tests/test_data/json/data.bad.invalid_keys.json"
+    )
+    assert (
+        str(exec_info.value)
+        == f"JSON file at path {file_path} must contain the field 'content'"
+    )
+
+
+def test_load_json_files_raises_not_a_list():
+    keys_to_load = ["content", "title"]
+    loader = CustomJSONLoader
+    with pytest.raises(ValueError) as exec_info:
+        load_structured_files(
+            file_format="JSON",
+            language=None,
+            loader=loader,
+            folder_path=os.path.abspath(
+                "rag_experiment_accelerator/doc_loader/tests/test_data/json"
+            ),
+            chunk_size=1000,
+            overlap_size=200,
+            glob_patterns=["not_a_list.json"],
+            loader_kwargs={
+                "keys_to_load": keys_to_load,
+            },
+        )
+
+    file_path = os.path.abspath(
+        "rag_experiment_accelerator/doc_loader/tests/test_data/json/data.bad.not_a_list.json"
+    )
+    assert (
+        str(exec_info.value)
+        == f"JSON file at path: {file_path} must be a list of object and expects each object to contain the fields ['content', 'title']"
+    )
