c7e0fe2659e0ce5ae3816d784074e8c0accaa6bc
Yomi Lajide <yolajide@microsoft.com>
2024-01-17 09:39:31 -0500

    remove used patches in tests

--- rag_experiment_accelerator/run/tests/test_qa_generation.py
+++ rag_experiment_accelerator/run/tests/test_qa_generation.py
@@ -3,26 +3,6 @@ from pandas import DataFrame
 from rag_experiment_accelerator.run.qa_generation import run
 from unittest import TestCase
 
-# @patch('rag_experiment_accelerator.run.qa_generation.get_default_az_cred')
-# @patch('rag_experiment_accelerator.run.qa_generation.load_documents')
-# @patch('rag_experiment_accelerator.run.qa_generation.Config')
-# @patch('os.makedirs')
-# def test_run(mock_makedirs, mock_config, mock_load_documents, mock_get_default_az_cred):
-#     # Arrange
-#     mock_config.return_value = Config('test_dir')
-#     mock_get_default_az_cred.return_value = 'test_cred'
-#     mock_load_documents.return_value = 'test_docs'
-#     mock_makedirs.side_effect = None
-
-#     # Act
-#     run('test_dir')
-
-#     # Assert
-#     mock_makedirs.assert_called_once_with('test_dir/artifacts_dir', exist_ok=True)
-#     mock_config.assert_called_once_with('test_dir')
-#     mock_get_default_az_cred.assert_called_once()
-#     mock_load_documents.assert_called_once_with('test_dir/DATA_FORMATS', 'test_dir/data_dir', 2000, 0)
-
 
 @patch("rag_experiment_accelerator.run.qa_generation.create_data_asset")
 @patch("rag_experiment_accelerator.run.qa_generation.generate_qna")
@@ -60,9 +40,6 @@ def test_run_success(
     mock_create_data_asset.assert_called_once()
 
 
-@patch("rag_experiment_accelerator.run.qa_generation.create_data_asset")
-@patch("pandas.DataFrame.to_json")
-@patch("rag_experiment_accelerator.run.qa_generation.generate_qna")
 @patch("os.makedirs")
 @patch("rag_experiment_accelerator.run.qa_generation.load_documents")
 @patch("rag_experiment_accelerator.run.qa_generation.get_default_az_cred")
@@ -72,9 +49,6 @@ def test_run_makedirs_exception(
     mock_get_default_az_cred,
     mock_load_documents,
     mock_makedirs,
-    mock_generate_qna,
-    mock_to_json,
-    mock_create_data_asset,
 ):
     # Arrange
     mock_config.return_value = MagicMock()
