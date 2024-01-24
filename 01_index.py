e7af36e8d0c288ea3faa2fbd25516f683eaaff1b
kjeffc <jeffrey159@gmail.com>
2024-01-24 15:13:36 -0800

    257 config params validation tests (#286)
    * initial tests
    
    * initial config validation
    
    * separate tests
    
    * update tests
    
    * updating config validation
    
    * run and tests
    
    * fix evaluation
    
    * removing chunk size

--- 01_index.py
+++ 01_index.py
@@ -4,4 +4,5 @@ from rag_experiment_accelerator.run.index import run
 if __name__ == "__main__":
     config_directory = ArgumentParser().get_directory_arg()
     data_directory = ArgumentParser().get_data_directory_arg()
-    run(config_directory, data_directory)
+    filename = ArgumentParser().get_config_filename_arg()
+    run(config_directory, data_directory, filename)
