51ba683b01055b275f3d1fbd1d1f4346c56334fd
Shane Peckham <shanepeckham@live.com>
2024-03-10 11:36:06 +0900

    Mocks

--- rag_experiment_accelerator/run/tests/test_qa_generation.py
+++ rag_experiment_accelerator/run/tests/test_qa_generation.py
@@ -25,6 +25,8 @@ def test_run_success(
     mock_generate_qna.return_value = mock_df
     mock_cluster = MagicMock()
     mock_read_csv = MagicMock()
+    mock_config.SAMPLE_DATA = False
+    all_docs = MagicMock()
     # Act
     run("test_dir")
 
@@ -36,7 +38,7 @@ def test_run_success(
     mock_generate_qna.assert_called_once()
     mock_df.to_json.assert_called_once()
     mock_create_data_asset.assert_called_once()
-    mock_cluster.assert_not_called()
+    mock_cluster.assert_called_once_with(all_docs, "test_dir", mock_config)
     mock_read_csv.assert_not_called()
 
 
