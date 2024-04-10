8e505904305361f7489aa02abbb9fcb83ccffc2a
Yuval Yaron <yuvalyaron@microsoft.com>
2024-04-10 14:27:34 +0000

    fix NullCheckpoint

--- rag_experiment_accelerator/checkpoint/null_checkpoint.py
+++ rag_experiment_accelerator/checkpoint/null_checkpoint.py
@@ -2,6 +2,9 @@ from rag_experiment_accelerator.checkpoint.checkpoint import Checkpoint
 
 
 class NullCheckpoint(Checkpoint):
+    def __init__(self):
+        pass
+
     def exists(self):
         pass
 
