f3372a618fc0a0fd82b6d1c50f07dbf770b186a2
Guy Bertental <guybartal@gmail.com>
2024-04-16 08:36:45 +0000

    Update source path in customJsonLoader.py

--- rag_experiment_accelerator/doc_loader/customJsonLoader.py
+++ rag_experiment_accelerator/doc_loader/customJsonLoader.py
@@ -52,7 +52,7 @@ class CustomJSONLoader(BaseLoader):
                     page_content.append(data_dict)
 
             metadata = {
-                "source": str(self.file_path).replace(str(Path.cwd()), ""),
+                "source": str(self.file_path),
             }
 
             docs.append(Document(page_content=str(page_content), metadata=metadata))
