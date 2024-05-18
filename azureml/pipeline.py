06bd1bc7bd59ed085843d7793e7596166bb12787
Guy Bertental <guybartal@gmail.com>
2024-05-18 17:26:13 +0000

    chore: Remove unused import in pipeline.py

--- azureml/pipeline.py
+++ azureml/pipeline.py
@@ -13,11 +13,13 @@ from rag_experiment_accelerator.config.paths import (
     formatted_datetime_suffix,
 )  # noqa: E402
 from rag_experiment_accelerator.config.environment import Environment  # noqa: E402
-from rag_experiment_accelerator.config.config import Config, ExecutionEnvironment  # noqa: E402
+from rag_experiment_accelerator.config.config import (
+    Config,
+    ExecutionEnvironment,
+)  # noqa: E402
 from rag_experiment_accelerator.config.index_config import IndexConfig  # noqa: E402
 from rag_experiment_accelerator.utils.auth import get_default_az_cred  # noqa: E402
 from rag_experiment_accelerator.utils.logging import get_logger  # noqa: E402
-from rag_experiment_accelerator.config.paths import mlflow_run_name  # noqa: E402
 
 
 project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
