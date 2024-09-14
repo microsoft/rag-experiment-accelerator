f7a69f6416842d4e38ca01c7cd8fe9fe99016af8
Prasanna Muralidharan <prmura@microsoft.com>
2024-09-14 01:41:48 +0000

    adding types

--- rag_experiment_accelerator/evaluation/plain_metrics.py
+++ rag_experiment_accelerator/evaluation/plain_metrics.py
@@ -1,12 +1,21 @@
 import evaluate
-import textdistance
 from rapidfuzz import fuzz
-
-algorithms = textdistance.algorithms
+from rapidfuzz import distance as d
+from textdistance import algorithms as alg
 
 
 # https://huggingface.co/spaces/evaluate-metric/bleu
-def bleu(predictions, references):
+def bleu(predictions: list[str], references: list[str]) -> float:
+    """
+    Computes the BLEU score between a list of candidate translations and a list of reference translations.
+
+    Args:
+        predictions (list): A list of candidate translations.
+        references (list): A list of reference translations.
+
+    Returns:
+        float: The BLEU score between the candidate and reference translations.
+    """
     bleu = evaluate.load("bleu")
 
     results = bleu.compute(predictions=predictions, references=references, max_order=2)
@@ -14,98 +23,97 @@ def bleu(predictions, references):
     return results["bleu"] * 100
 
 
-def fuzzy(doc1, doc2):
+def fuzzy(str1: str, str2: str, match_type: str) -> float:
     """
-    Calculates the fuzzy score between two documents.
-
-    Parameters:
-        doc1 (str): The first document to compare.
-        doc2 (str): The second document to compare.
-
-    Returns:
-        int: The fuzzy score between the two documents.
-    """
-    differences = fuzzy_compare_values(doc1, doc2)
-
-    return int(sum(differences) / len(differences))
-
-
-def fuzzy_compare_values(value1, value2) -> list[float]:
-    """
-    Compares two values using fuzzy string matching and appends the similarity score to a list of differences.
+    Compares two strings using fuzzy string matching and returns a similarity score.
 
     Args:
-        value1 (str): The first value to compare.
-        value2 (str): The second value to compare.
+        str1 (str): The first string to compare.
+        str2 (str): The second string to compare.
+        match_type (str): The type of fuzzy string matching to use. Options include:
+            - 'ratio'
+            - 'token_set_ratio'
+            - 'token_sort_ratio'
+            - 'partial_ratio'
+            - 'partial_token_sort_ratio'
+            - 'partial_token_set_ratio'
 
     Returns:
         A list of the similarity scores.
+
+    Raises:
+        ValueError: If the match type is not recognized.
     """
-    similarity_score = [fuzz.token_set_ratio(value1, value2)]
+    try:
+        fuzzy_match_fn = getattr(fuzz, match_type)
+    except AttributeError:
+        raise ValueError(f"Match type '{match_type}' is not recognized.")
+
+    similarity_score = fuzzy_match_fn(str1, str2)
     return similarity_score
 
 
-def levenshtein(value1, value2):
+def levenshtein(str1: str, str2: str) -> int:
     """
     Calculates the Levenshtein distance between two strings and returns the normalized similarity score as a percentage.
 
     Args:
-        value1 (str): The first string to compare.
-        value2 (str): The second string to compare.
+        str1 (str): The first string to compare.
+        str2 (str): The second string to compare.
 
     Returns:
         int: The normalized similarity score as a percentage.
     """
-    score = int(algorithms.levenshtein.normalized_similarity(value1, value2) * 100)
+    score = d.Levenshtein.normalized_similarity(str1, str2) * 100
     return score
 
 
-def jaccard(value1, value2):
+def jaccard(str1: str, str2: str) -> float:
     """
     Calculates the Jaccard similarity score between two sets of values.
 
     Args:
-        value1 (set): The first set of values.
-        value2 (set): The second set of values.
+        str1 (set): The first set of values.
+        str2 (set): The second set of values.
 
     Returns:
         int: The Jaccard similarity score between the two sets of values, as a percentage.
     """
-    score = int(algorithms.jaccard.normalized_similarity(value1, value2) * 100)
+    score = alg.jaccard.normalized_similarity(str1, str2) * 100
     return score
 
 
-def hamming(value1, value2):
+def hamming(str1: str, str2: str) -> int:
     """
     Calculates the Hamming similarity score between two values.
 
     Args:
-        value1 (str): The first value to compare.
-        value2 (str): The second value to compare.
+        str1 (str): The first value to compare.
+        str2 (str): The second value to compare.
 
     Returns:
         int: The Hamming similarity score between the two values, as a percentage.
     """
-    score = int(algorithms.hamming.normalized_similarity(value1, value2) * 100)
+    score = int(d.Hamming.normalized_similarity(str1, str2) * 100)
     return score
 
 
-def jaro_winkler(value1, value2):
+def jaro_winkler(str1: str, str2: str) -> int:
     """
     Calculates the Jaro-Winkler similarity score between two strings.
 
     Args:
-        value1 (str): The first string to compare.
-        value2 (str): The second string to compare.
+        str1 (str): The first string to compare.
+        str2 (str): The second string to compare.
 
     Returns:
         int: The Jaro-Winkler similarity score between the two strings, as an integer between 0 and 100.
     """
-    score = int(algorithms.jaro_winkler.normalized_similarity(value1, value2) * 100)
+    score = int(d.JaroWinkler.normalized_similarity(str1, str2) * 100)
     return score
 
 
-def cosine(value1, value2):
+def cosine(str1: str, str2: str) -> int:
     """
     Calculates the cosine similarity (Ochiai coefficient) between two strings
     using token-frequency vectors
@@ -113,41 +121,41 @@ def cosine(value1, value2):
     https://en.wikipedia.org/wiki/Cosine_similarity.
 
     Args:
-        value1 (list): The first vector.
-        value2 (list): The second vector.
+        str1 (list): The first vector.
+        str2 (list): The second vector.
 
     Returns:
         int: The cosine similarity score between the two vectors, as a percentage.
     """
-    score = int(algorithms.cosine.normalized_similarity(value1, value2) * 100)
+    score = int(alg.Cosine.normalized_similarity(str1, str2) * 100)
     return score
 
 
-def lcsseq(value1, value2):
+def lcsseq(str1: str, str2: str) -> int:
     """
     Computes the longest common subsequence (LCS) similarity score between two input strings.
 
     Args:
-        value1 (str): The first input string.
-        value2 (str): The second input string.
+        str1 (str): The first input string.
+        str2 (str): The second input string.
 
     Returns:
         int: The LCS similarity score between the two input strings, as a percentage (0-100).
     """
-    score = int(algorithms.lcsseq.normalized_similarity(value1, value2) * 100)
+    score = int(d.LCSseq.normalized_similarity(str1, str2) * 100)
     return score
 
 
-def lcsstr(value1, value2):
+def lcsstr(str1: str, str2: str) -> int:
     """
     Calculates the longest common substring (LCS) similarity score between two strings.
 
     Args:
-        value1 (str): The first string to compare.
-        value2 (str): The second string to compare.
+        str1 (str): The first string to compare.
+        str2 (str): The second string to compare.
 
     Returns:
         int: The LCS similarity score between the two strings, as a percentage (0-100).
     """
-    score = int(algorithms.lcsstr.normalized_similarity(value1, value2) * 100)
+    score = int(alg.lcsstr.normalized_similarity(str1, str2) * 100)
     return score
