bf519c9fe2ab266163e035099bc7e32dab9145c3
Thomas Conté <tconte@microsoft.com>
2024-09-16 23:17:03 +0200

    Update dev container for unstructured (#728)
    Update the dev container to support PDF text extraction from images,
    using `unstructured[pdf]`.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Install system dependencies for unstructured
+sudo apt-get update && sudo apt-get install -y libgl1-mesa-glx poppler-utils tesseract-ocr
+
+# Install Python dependencies
 pip install --upgrade pip
 
 pip install -r requirements.txt
