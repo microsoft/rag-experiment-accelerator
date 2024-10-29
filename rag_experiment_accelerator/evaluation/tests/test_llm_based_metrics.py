5eb3896a62ba1aee0b92793c62f0c1b166649220
Bill DeRusha <444835+bderusha@users.noreply.github.com>
2024-10-29 18:50:40 +0000

    Fix unit test

--- rag_experiment_accelerator/evaluation/tests/test_llm_based_metrics.py
+++ rag_experiment_accelerator/evaluation/tests/test_llm_based_metrics.py
@@ -46,9 +46,28 @@ def test_llm_context_precision(mock_generate_response):
 
 @patch("rag_experiment_accelerator.evaluation.eval.ResponseGenerator")
 def test_llm_context_recall(mock_generate_response):
-    mock_generate_response.generate_response.return_value = (
-        '"Attributed": "1"   "Attributed": "1"   "Attributed": "1"   "Attributed": "0"'
-    )
+    mock_generate_response.generate_response.return_value = [
+        {
+            "statement_1": "Test statement 1",
+            "reason": "The statement is in the context",
+            "attributed": "1",
+        },
+        {
+            "statement_2": "Test statement 2",
+            "reason": "The statement is in the context",
+            "attributed": "1",
+        },
+        {
+            "statement_3": "Test statement 3",
+            "reason": "The statement is in the context",
+            "attributed": "0",
+        },
+        {
+            "statement_4": "Test statement 4",
+            "reason": "The statement is in the context",
+            "attributed": "1",
+        },
+    ]
     question = "What is the name of the largest bone in the human body?"
     context = 'According to the Cleveland Clinic, "The femur is the largest and strongest bone in the human body. It can support as much as 30 times the weight of your body. The average adult male femur is 48 cm (18.9 in) in length and 2.34 cm (0.92 in) in diameter. The average weight among adult males in the United States is 196 lbs (872 N). Therefore, the adult male femur can support roughly 6,000 lbs of compressive force."'
     answer = "The largest bone in the human body is the femur, also known as the thigh bone. It is about 19.4 inches (49.5 cm) long on average and can support up to 30 times the weight of a person’s body."
